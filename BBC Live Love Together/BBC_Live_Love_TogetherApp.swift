605e248f60d1052acf3ae3415517cccfd8dcc154
Omar Amoako <omar.amoako@yahoo.co.uk>
2022-06-07 16:54:38 +0100

    Initial Commit

---
+++ BBC Live Love Together/BBC_Live_Love_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBC_Live_Love_TogetherApp.swift
+//  BBC Live Love Together
+//
+//  Created by Omar Amoako on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BBC_Live_Love_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBC Live Love Together/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBC Live Love Together
+//
+//  Created by Omar Amoako on 07/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
